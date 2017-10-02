8aea410a4b80b51ed5fa57a66a304445817ea76a
Laurent <ranlot75@gmail.com>
2017-10-02 17:47:18 +0300

    loop in movie maker

--- movieMaker.sh
+++ movieMaker.sh
@@ -2,7 +2,7 @@
 
 function movieMaker {
   echo $1
-  convert -loop 1 -delay 10 $(ls $1 | sort -n -t. -k2) $2
+  convert -loop 0 -delay 10 $(ls $1 | sort -n -t. -k2) $2
 }
 
 movieMaker "framesDir/decisionBoundaries.*.png" "hidden.anim.gif"
